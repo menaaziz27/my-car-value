0e9b355662d20bbbf68281ac2802cfa8a0d60e43
Mena Aziz <menaaziz27@gmail.com>
2022-09-18 08:15:05 +0200

    interceptor to get current signed in user

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -9,16 +9,21 @@ import { UsersService } from '../users.service';
 
 @Injectable()
 export class CurrentUserInterceptor implements NestInterceptor {
+  // when this interceptor being executed ?
+  // before any request reach any controller in our app
+  // this interceptor is made to use dependency injection of user service which we cannot use in normal decorators
   constructor(private userService: UsersService) {}
   async intercept(context: ExecutionContext, handler: CallHandler<any>) {
     const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+    const { userId } = request.session || {};
 
+    // if there is a userId find the user and attach him to request
     if (userId) {
       const user = await this.userService.findOne(userId);
       request.currentUser = user;
     }
 
+    // if userId is not found .. handle the route normally
     return handler.handle();
   }
 }
