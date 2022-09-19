ef0e3a73d205998ff3cbd28a9cc13707025b662f
Mena Aziz <menaaziz27@gmail.com>
2022-09-19 05:33:28 +0200

    fixed wrong attribute

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -6,11 +6,12 @@ export class AdminGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
+
     // interceptors always gonna run after any middleware and after any guards .. that why admin guard always return false -> because the interceptor of current-user always run after the guard so there's no request.currentUser attached in the request
     if (!request.currentUser) {
       return false; // check if the user is signed in first
     }
 
-    return request.currentUser.admin; // if the user is admin ok proceeed to the handler .. if false reject the request
+    return request.currentUser.isAdmin; // if the user is admin ok proceeed to the handler .. if false reject the request
   }
 }
